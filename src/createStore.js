ffbbb31fbf8d18fc78de9399459a323445fc740f
ipc103 <iancandysemail@gmail.com>
2016-11-01 14:25:05 -0400

    remove solution

--- src/createStore.js
+++ src/createStore.js
@@ -1,25 +1,3 @@
-function createStore(reducer){
-  let state;
-  let listeners = [];
-
-  const getState = () => state;
-
-  const subscribe = (listener) => listeners.push(listener);
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach((listener) => {
-      listener();
-    })
-  }
-
-  dispatch({});
-
-  return {
-    getState,
-    subscribe,
-    dispatch
-  }
+export default function createStore(reducer){
+// your code here!
 }
-
-export default createStore;
