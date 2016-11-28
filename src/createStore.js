9353cb7cbae534e315431a5886ba3d8bc09c8f6d
satub <satu.barnhill@gmail.com>
2016-11-28 12:05:44 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,3 +1,26 @@
 export default function createStore(reducer){
 // your code here!
+  let listeners = [];
+  let state;
+
+  const getState = () => {
+    const stuff = reducer(state, {action: {type: "DEFAULT"}});
+    return stuff;
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    // console.log(state);
+    listeners.forEach( listener => listener() )
+  };
+
+  return {
+    getState: getState,
+    subscribe: subscribe,
+    dispatch: dispatch
+  }
 }
