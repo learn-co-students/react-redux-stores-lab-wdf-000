c2a3f470338e2b591981a2246c48c82426de9659
Avidor Turkewitz <avi.turkewitz@flatironschool.com>
2016-12-15 22:18:09 +0000

    Solution

--- src/createStore.js
+++ src/createStore.js
@@ -1,3 +1,26 @@
 export default function createStore(reducer){
 // your code here!
+  let state;
+  let listeners = [];
+  
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  }
+  
+  const dispatch = (action) => {
+   state = reducer(state, action)
+   listeners.forEach( listener => listener() ) 
+  }
+  
+  dispatch({type:"Initial Action"})
+  
+  const getState = () => {
+    return state
+  }
+  
+  return{
+    getState: getState,
+    dispatch: dispatch,
+    subscribe, subscribe
+  }
 }
