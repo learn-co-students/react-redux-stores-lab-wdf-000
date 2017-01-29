6bf9951f014bc615a2de79baaaf57383530998d0
Flatiron School <github email address>
2017-01-29 18:27:08 -0500

    done

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,21 @@
 export default function createStore(reducer){
 // your code here!
+  let state;
+  let listeners = [];
 
+  const getState = () => { return reducer(state, reducer); };
+  // const getState = () => { return this.state}
+
+  const subscribe = (listener) => listeners.push(listener);
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach( listener => listener())
+  };
+
+  return {
+    getState: getState,
+    dispatch: dispatch,
+    subscribe: subscribe
+  }
 }
